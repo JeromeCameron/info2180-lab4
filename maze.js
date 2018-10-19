cd76ed3f5e7746abe2d656619bd17e5d757c22e1
Jerome Cameron <jeromecameron@gmail.com>
2018-10-18 23:46:55 -0500

    exercise three complete

--- maze.js
+++ maze.js
@@ -2,15 +2,29 @@
 
 window.onload = function(){
 
+    //highlight first wall if touched
     let trigger = document.getElementById("boundary1");
-
     trigger.addEventListener("mouseover", function(){
         trigger.classList.add('youlose');
     });
 
+    //highlight any wall touched
     let triggerAny = document.querySelectorAll("div#maze > div.boundary");
-    console.log(triggerAny);
+
     triggerAny.forEach(item => item.addEventListener("mouseover", function(){
         item.classList.add('youlose');
     }));
+
+    //check if winner
+    let finish = document.getElementById('end');
+    finish.addEventListener("mouseover", function(){
+        let tracker = 0;
+        triggerAny.forEach(function(item){
+            if (item.classList.contains('youlose')){
+                 tracker = tracker + 1;
+            }
+        });
+        tracker === 0 ? alert("You Win!") : alert("You Lose!");
+    });
+
 }
